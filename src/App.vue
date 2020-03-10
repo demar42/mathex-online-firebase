c31afba79a6bdd9ac15baae2233f1758898cd905
Daniel Mar <mardaniel42@gmail.com>
2020-03-11 02:09:07 +1300

    added title component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container h-100">
+    <Title></Title>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Title from '@/components/Title'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Title
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Title.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container m-2 p-2 border-bottom">
+        <span class="mb-0 display-4">Mathex Online</span>
+        <span class="ml-4 text-muted">A web-app by Daniel Mar</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Title'
+}
+</script>
