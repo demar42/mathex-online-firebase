01bcaa4cb68ee65d6baf9d48d5c1e4ed92242272
Daniel Mar <mardaniel42@gmail.com>
2020-03-16 00:59:03 +1300

    added vuefire

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import {rtdbPlugin} from 'vuefire'
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -8,6 +9,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 require('./firebase.config.js')
 
 Vue.config.productionTip = false
+
+Vue.use(rtdbPlugin)
 
 new Vue({
   render: h => h(App),
