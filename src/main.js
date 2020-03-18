332f3da72e58584437ec3fcd275d9a7c2d032ee1
Daniel Mar <mardaniel42@gmail.com>
2020-03-19 00:59:02 +1300

    added vue-cookie

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import {rtdbPlugin} from 'vuefire'
+import VueCookie from 'vue-cookie'
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -11,6 +12,7 @@ require('./firebase.config.js')
 Vue.config.productionTip = false
 
 Vue.use(rtdbPlugin)
+Vue.use(VueCookie)
 
 new Vue({
   render: h => h(App),
