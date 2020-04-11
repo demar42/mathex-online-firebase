bc4c14d96f0f174bd6989448d6732eb4c0ce598c
Daniel Mar <mardaniel42@gmail.com>
2020-04-11 13:24:51 +1200

    added sentry.io for error tracking

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
 import {rtdbPlugin} from 'vuefire'
+
 import VueCookie from 'vue-cookie'
+
+import * as Sentry from '@sentry/browser'
+import * as Integrations from '@sentry/integrations'
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -13,6 +18,11 @@ Vue.config.productionTip = false
 
 Vue.use(rtdbPlugin)
 Vue.use(VueCookie)
+
+Sentry.init({
+  dsn: 'https://14d985b74ef84461a63ee2fee5d1ee1d@o374896.ingest.sentry.io/5196769',
+  integrations: [new Integrations.Vue({Vue, attachProps: true})],
+})
 
 new Vue({
   render: h => h(App),
